1dc6961ab11e7a4ac24d3c75b53754b2da7f209e
aimansae <saeedaimann@gmail.com>
2022-09-14 13:54:53 +0000

    Add test and relative keys

---
+++ script/game.js
@@ -0,0 +1,11 @@
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: []
+}
+
+//must export {if there are more that 1 object} check line 10 in test.js
+module.exports = {
+    game
+}
---
+++ script/tests/game.test.js
@@ -0,0 +1,38 @@
+/**
+ * @jest-environment jsdom
+ */
+
+//type in terminal:
+// npm init (type jest as test command) 
+// npm install --save-dev jest
+// npm install -D jest-environment-jsdom
+
+//mandatory
+
+const {game} = require('../game');
+
+beforeAll(() => {
+    //setting DOM mock always like this!
+
+    let fs = require('fs');
+    let fileContents = fs.readFileSync('index.html', 'utf-8');
+    document.open();
+    document.write(fileContents);
+    document.close();
+})
+
+describe('game object contains correct keys', () => {
+    test('score key exists', () => {
+        expect('score' in game).toBe(true);
+    });
+    test('currentGame key exists', () => {
+        expect('currentGame' in game).toBe(true);
+    });
+    test('playerMoves key exists', () =>{
+        expect('playerMoves' in game).toBe(true);
+    })
+    test('choices key exists', () =>{
+        expect('choices' in game).toBe(true);
+    })
+
+});
