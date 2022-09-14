2e217704974d5a548f3efe27ade682b29635d7eb
aimansae <saeedaimann@gmail.com>
2022-09-14 14:01:48 +0000

    made test for elements in array  choices

--- script/game.js
+++ script/game.js
@@ -2,10 +2,8 @@ let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: []
+    choices: ['button1', 'button2','button3', 'button4'],
 }
 
 //must export {if there are more that 1 object} check line 10 in test.js
-module.exports = {
-    game
-}
+module.exports = {game};
--- script/tests/game.test.js
+++ script/tests/game.test.js
@@ -9,7 +9,9 @@
 
 //mandatory
 
-const {game} = require('../game');
+const {
+    game
+} = require('../game');
 
 beforeAll(() => {
     //setting DOM mock always like this!
@@ -28,11 +30,14 @@ describe('game object contains correct keys', () => {
     test('currentGame key exists', () => {
         expect('currentGame' in game).toBe(true);
     });
-    test('playerMoves key exists', () =>{
+    test('playerMoves key exists', () => {
         expect('playerMoves' in game).toBe(true);
     })
-    test('choices key exists', () =>{
+    test('choices key exists', () => {
         expect('choices' in game).toBe(true);
+    })
+    test('choices contains the correct ID', () => {
+        expect(game.choices).toEqual(['button1', 'button2','button3', 'button4']);
     })
 
 });
